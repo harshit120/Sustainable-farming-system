d1e8ea6fb71ab6594b49b62d0130b7cc73030f04
kanishk123agarwal <kanishkagarwal208@gmail.com>
2023-11-17 20:31:30 +0530

    update user schema

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -1,11 +1,31 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
-  email: String,
-  region: String,
-  landSize: Number,
+  username: {
+    type:String,
+    required:true, 
+  },
+  password: {
+    type:String,
+    required:true 
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true
+  },
+  region: {
+    type:String,
+    required:true
+  },
+  landSize: {
+    type:Number,
+    required:true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const User = mongoose.model("User", userSchema);
