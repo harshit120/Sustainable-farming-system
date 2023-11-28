85a8e8e911ed39afdb0d49e1cba313e74665d414
harshilsodani <harshilsodani@users.noreply.github.com>
2023-11-28 18:41:59 +0530

    Fix API route order

--- Backend/app.js
+++ Backend/app.js
@@ -11,11 +11,11 @@ const policyRoutes = require('./routes/policyRoutes');
 const adviceRoutes = require('./routes/adviceRoutes');
 const purchaseRoutes = require('./routes/purchaseRoutes');
 
-app.use('api/v1/users', userRoutes);
-app.use('api/v1/fertilizers', fertilizerRoutes);
-app.use('api/v1/crops', cropRoutes);
-app.use('api/v1/policies', policyRoutes);
-app.use('api/v1/advices', adviceRoutes);
+app.use('/api/v1/fertilizers', fertilizerRoutes);
+app.use('/api/v1/crops', cropRoutes);
+app.use('/api/v1/policies', policyRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/advices', adviceRoutes);
 app.use('/api/v1/users/:userId/purchases', purchaseRoutes);
 
 module.exports = app;
