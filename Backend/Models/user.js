27f16c0ae0b47f2d76cafd6e7ca71bc9c30ddfb3
Dhairya Gupta <dhairyagupta0307@gmail.com>
2023-12-03 17:39:44 +0530

    login

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: {
@@ -26,6 +27,13 @@ const userSchema = new mongoose.Schema({
     type: Date,
     default: Date.now
   }
+});
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -4,6 +4,9 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+router.post('/signup', userController.signup);
+router.post('/login', userController.login);
+
 router
   .route('/')
   .get(userController.getAllUsers)
